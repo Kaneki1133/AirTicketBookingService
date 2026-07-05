4a850ef15e3f297f4e7c640ff0f4ecc17eafdb11
Kaneki1133 <shloktiwari1133@gmail.com>
2026-07-06 00:19:56 +0530

    Added rabbit mq basic config setup

--- src/controller/booking-controller.js
+++ src/controller/booking-controller.js
@@ -2,15 +2,57 @@ const { BookingService } = require(`../service/index`);
 const { StatusCodes } = require(`http-status-codes`);
 const bookingService = new BookingService();
 
-const create = async (req , res) =>{
+const { createChannel, publishMessage } = require(`../utils/messageQueue`);
+const { REMINDER_BINDING_KEY } = require(`../config/serverConfig`);
+
+class BookingController {
+    constructor() {
+    }
+
+    async sendMessageToQueue(req, res) {
+        const channel = await createChannel();
+        const data = { message: 'Success' };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message: 'Successfully published the event',
+        });
+    }
+
+    async create(req, res) {
+        try {
+            const response = await bookingService.createBooking(req.body);
+            console.log("FROM Booking Controller ", response);
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "SuccessFully Booking Completed",
+                err: {},
+                data: response
+            });
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                err: error.explanation,
+                data: {}
+            });
+        }
+    }
+}
+module.exports = BookingController;
+
+
+
+/**
+ * 
+ *     async create(req, res) => {
     try {
         const response = await bookingService.createBooking(req.body);
         console.log("FROM Booking Controller ", response);
         return res.status(StatusCodes.OK).json({
             success: true,
-            message:"SuccessFully Booking completed",
-            err:{},
-            data:response
+            message: "SuccessFully Booking completed",
+            err: {},
+            data: response
         });
     } catch (error) {
         return res.status(error.statusCodes).json({
@@ -21,6 +63,5 @@ const create = async (req , res) =>{
         });
     }
 }
-module.exports ={
-    create,
-}
+ * 
+ */
